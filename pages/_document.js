e76088b4909f21c79d24ed471743d1954eec9de0
Laptop Ravi <laptop93193@gmail.com>
2026-09-09 12:40:50 +0530

    perf: scope Material Symbols loading

--- pages/_document.js
+++ pages/_document.js
@@ -33,11 +33,6 @@ export default function Document() {
 
         <link
           rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap"
-        />
-
-        <link
-          rel="stylesheet"
           href="/assets/vendor/bootstrap/css/bootstrap.min.css"
         />
 
